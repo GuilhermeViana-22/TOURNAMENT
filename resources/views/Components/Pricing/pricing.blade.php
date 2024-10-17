9ddb4fa7b44b69f15a657908d6018d405ee7fa24
GuilhermeViana-22 <gguicido.viana@gmail.com>
2024-10-17 00:06:40 -0300

    ajustes

--- resources/views/Components/Pricing/pricing.blade.php
+++ resources/views/Components/Pricing/pricing.blade.php
@@ -1,64 +1,44 @@
-<div class="container" id="pricing">
-    <div class="mt-4 d-flex justify-content-center">
-          <img src="{{ asset('/asset/lendas/val.png') }}" alt="Brawlhalla - Val" class="img-fluid m-2" style="max-width: 150px;">
-   </div>
-    <!-- Título e Descrição -->
-    <div class="row">
-        <div class="col-md-12 mx-auto text-center">
-            <h1 class="text-white heading-black">Inscrição no Torneio</h2>
-            <p class="text-light lead">Preço simples - Pagamento em Reais (R$)</p>
-            <h4 class="text-warning mt-4">Prepare-se para a ação! 🎮</h4>
-            <p class="text-light">🗓️ Data do Torneio: 23 de Novembro às 13h!</p>
-            <p class="text-light">🌟 Transmissão ao vivo de todas as emoções!</p>
-        </div>
+<div class="container my-5" id="pricing">
+    <!-- Cabeçalho com imagem e título -->
+    <div class="text-center mb-4">
+        <h1 class="text-white mt-3">Inscrição no Torneio</h1>
+        <p class="text-light lead">Preço simples - Pagamento em Reais (R$) via LIVE PIX</p>
+        <h4 class="text-warning mt-3">Prepare-se para a ação!</h4>
     </div>
-    <!-- Alerta Importante -->
-    <div class="alert alert-warning mt-4" role="alert">
-        <strong>Importância das Regras:</strong> As regras são fundamentais para garantir um campeonato saudável e divertido para todos os participantes. A colaboração de cada um é essencial!
-    </div>
-    <div class="row pt-5">
-        <!-- Tabela de Preços -->
-        <div class="col-lg-5 col-md-5 mb-5">
-            <div class="card  glowing-card">
-                <div class="card-body">
-                    <h3 class="card-title pt-3">Inscrição Torneio</h3>
-                    <h2 class="card-title text-info mb-0 pt-4">R$ 10,00</h2>
-                    <ul class="list-unstyled pricing-list">
-                        <li><i class="fas fa-microphone mt-3" style="margin-right: 10px;"></i>Transmissão ao vivo</li>
-                        <li><i class="fas fa-video mt-3" style="margin-right: 10px;"></i>Clipes de destaques no evento</li>
-                        <li><i class="fas fa-trophy mt-3" style="margin-right: 10px;"></i>Prêmios para os 3 primeiros colocados</li>
-                        <li><i class="fas fa-user-plus mt-3" style="margin-right: 10px;"></i>Inscrição para o torneio 2x2</li>
-                        <li><i class="fab fa-discord mt-3" style="margin-right: 10px;"></i>Comunicação através do Discord</li>
-                    </ul>
-                    <a href="#" class="btn btn-info">Realizar Pagamento</a>
-                </div>
-            </div>
-        </div>
+   <div class="row justify-content-center">
+    <div class="col-md-10 text-center mb-5">
+        <div class="p-5 rounded shadow-lg" style="background-color: #2d2d2d; border-radius: 20%;">
+            <img src="{{ asset('logo/shodwe.png') }}" alt="Brawlhalla - Val" class="img-fluid mb-3" style="max-width: 150px;">
+            <h2 class="text-light mb-3"><i class="fas fa-calendar-alt"></i> Data do Torneio: 23 de Novembro</h2>
+            <h3 class="text-light"><i class="fas fa-clock"></i> Horário: 13h</h3>
+            <p class="text-light mt-4">Assista a transmissão ao vivo nas plataformas:</p>
 
-        <!-- Área de Regras -->
-        <div class="col-lg-7 col-md-7 mb-5">
-            <div class="card glowing-card">
-                <div class="card-body text-center">
-                    <h4 class="card-title">Regras do Torneio</h4>
-                    <ul class="list-unstyled text-left mt-3 pricing-list">
-                        <li><i class="fas fa-fist-raised" style="margin-right: 10px;"></i>Formato do Torneio: Eliminatórias 2x2 com partidas diretas.</li>
-                        <li><i class="fas fa-hourglass-start" style="margin-right: 10px;"></i>Duração das Partidas: Cada partida terá uma duração máxima de 5 minutos.</li>
-                        <li><i class="fas fa-gamepad" style="margin-right: 10px;"></i>Personagens: Todos os personagens disponíveis no jogo podem ser usados.</li>
-                        <li><i class="fas fa-shield-alt" style="margin-right: 10px;"></i>Limite de Vida: Cada jogador terá 3 vidas por partida.</li>
-                        <li><i class="fas fa-trophy" style="margin-right: 10px;"></i>Prêmios: Serão entregues prêmios para os 3 primeiros colocados.</li>
-                        <li><i class="fas fa-exchange-alt" style="margin-right: 10px;"></i>Desempate: Em caso de empate, será realizada uma partida extra para decidir o vencedor.</li>
-                        <li><i class="fas fa-comments" style="margin-right: 10px;"></i>Comunicação: Os jogadores devem estar conectados ao Discord durante as partidas.</li>
-                        <li><i class="fas fa-exclamation-triangle" style="margin-right: 10px;"></i>Comportamento: Respeito e fair play são obrigatórios. Jogadores que não seguirem estas diretrizes poderão ser desqualificados.</li>
-                        <li><i class="fas fa-video" style="margin-right: 10px;"></i>Gravação: Todas as partidas serão gravadas e poderão ser utilizadas para clipes de destaques.</li>
-                        <li><i class="fas fa-question-circle" style="margin-right: 10px;"></i>Dúvidas: Em caso de dúvidas, entre em contato com a equipe organizadora através do Discord.</li>
-                        <li><i class="fas fa-ban" style="margin-right: 10px;"></i>Ofensas e Brigas: Equipes que tentarem desequilibrar o campeonato através de ofensas ou brigas serão expulsas.</li>
-                    </ul>
+            <!-- Grid com 3 colunas -->
+            <div class="row text-center justify-content-center p-4">
+                <!-- Twitch -->
+                <div class="col-4 mb-4">
+                    <a href="https://www.twitch.tv/guilherme_viana_play" target="_blank" class="text-decoration-none">
+                        <i class="fab fa-twitch fa-3x text-purple bg-white p-4"></i>
+                
+                    </a>
+                </div>
+
+                <!-- YouTube -->
+                <div class="col-4 mb-4">
+                    <a href="https://www.youtube.com/@GuiVianaPlay" target="_blank" class="text-decoration-none">
+                        <i class="fab fa-youtube fa-3x text-danger bg-white  p-4"></i>
+                    </a>
+                </div>
+
+                <!-- Discord -->
+                <div class="col-4 mb-4">
+                    <a href="https://discord.gg/7GBSyNfADx" target="_blank" class="text-decoration-none">
+                        <i class="fab fa-discord fa-3x text-purple bg-white  p-4"></i>
+              
+                    </a>
                 </div>
             </div>
         </div>
     </div>
-
-
 </div>
-
-
+</div>
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -2,13 +2,13 @@
     <form method="POST" action="{{ route('register') }}">
         @csrf
 
-        <!-- Name -->
+        <!-- Nome -->
         <div>
-            <label for="name" class="block text-sm font-medium text-gray-700">Name</label>
+            <label for="name" class="block text-sm font-medium text-gray-700">Nome</label>
             <input id="name" class="block mt-1 w-full border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm" type="text" name="name" value="{{ old('name') }}" required autofocus autocomplete="name" />
         </div>
 
-        <!-- Email Address -->
+        <!-- Endereço de Email -->
         <div class="mt-4">
             <label for="email" class="block text-sm font-medium text-gray-700">Email</label>
             <input id="email" class="block mt-1 w-full border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm" type="email" name="email" value="{{ old('email') }}" required autocomplete="username" />
@@ -17,18 +17,18 @@
             @endif
         </div>
 
-        <!-- Password -->
+        <!-- Senha -->
         <div class="mt-4">
-            <label for="password" class="block text-sm font-medium text-gray-700">Password</label>
+            <label for="password" class="block text-sm font-medium text-gray-700">Senha</label>
             <input id="password" class="block mt-1 w-full border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm" type="password" name="password" required autocomplete="new-password" />
             @if ($errors->has('password'))
                 <span class="text-sm text-red-600 mt-2">{{ $errors->first('password') }}</span>
             @endif
         </div>
 
-        <!-- Confirm Password -->
+        <!-- Confirmar Senha -->
         <div class="mt-4">
-            <label for="password_confirmation" class="block text-sm font-medium text-gray-700">Confirm Password</label>
+            <label for="password_confirmation" class="block text-sm font-medium text-gray-700">Confirmar Senha</label>
             <input id="password_confirmation" class="block mt-1 w-full border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm" type="password" name="password_confirmation" required autocomplete="new-password" />
             @if ($errors->has('password_confirmation'))
                 <span class="text-sm text-red-600 mt-2">{{ $errors->first('password_confirmation') }}</span>
@@ -37,11 +37,11 @@
 
         <div class="flex items-center justify-end mt-4">
             <a class="underline text-sm text-gray-600 hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('login') }}">
-                Already registered?
+                Já está registrado?
             </a>
 
             <button type="submit" class="ms-4 inline-flex items-center px-4 py-2 bg-indigo-600 border border-transparent rounded-md font-semibold text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
-                Register
+                Registrar
             </button>
         </div>
     </form>
