dcbc4b380136a38492d7c4a3752a6abe2eae4554
GuilhermeViana-22 <gguicido.viana@gmail.com>
2024-10-20 18:33:21 -0300

    atualizando as refernecias

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,23 +5,15 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <link rel="icon" href="{{  asset('logo/shodwe_sem_fundo.png')  }}">
+    <link rel="icon" href="{{ asset('logo/shodwe_sem_fundo.png') }}">
     <title>Torneio Warriors of Valhalla</title>
+    
+    <!-- Stylesheets -->
+    <link rel="stylesheet" href="{{ asset('dist/assets/app.css') }}">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/1.1.3/sweetalert.min.css">
-    <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.bunny.net">
-    <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+    <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" rel="stylesheet">
-
-    <!-- Scripts -->
-    @vite([
-             'resources/css/app.css',
-                'resources/css/auth.css',
-                'resources/css/bracket.scss',
-                'resources/js/app.js',
-                'resources/js/main.js',
-                'resources/js/sweetalert2.js',
-    ])
 </head>
 
 <body class="font-sans text-dark-100 antialiased">
@@ -32,7 +24,7 @@
         @if (isset($header))
             <header class="bg-gray-800 shadow">
                 <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-                    <h1 class="text-white">{{ $header }}</h1> <!-- Mantendo texto branco para o header -->
+                    <h1 class="text-white">{{ $header }}</h1>
                 </div>
             </header>
         @endif
@@ -42,9 +34,11 @@
             {{ $slot }}
         </main>
     </div>
-</body>
 
-<script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script> 
-<script src="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/1.1.3/sweetalert.min.js"></script>
+    <!-- Scripts -->
+    <script src="{{ asset('dist/assets/app.js') }}"></script>
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/1.1.3/sweetalert.min.js"></script>
+</body>
 
 </html>
--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -5,39 +5,33 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <link rel="icon" href="{{  asset('logo/shodwe_sem_fundo.png')  }}">
+    <link rel="icon" href="{{ asset('logo/shodwe_sem_fundo.png') }}">
+    <link rel="stylesheet" href="{{ asset('dist/assets/app.css') }}">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/1.1.3/sweetalert.min.css">
-    <title>Torneio Warrios of Valhalla</title>
+    <title>Torneio Warriors of Valhalla</title>
 
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
-
-    <!-- Scripts -->
-    @vite([
-             'resources/css/app.css',
-                'resources/css/auth.css',
-                'resources/css/bracket.scss',
-                'resources/js/app.js',
-                'resources/js/main.js',
-                'resources/js/sweetalert2.js',
-    ])
 </head>
 
 <body class="font-sans text-dark-100 antialiased">
     <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0"
-    style="background-image: url('{{ asset('hero/background.png') }}'); background-size: cover; background-position: center; background-color: #2d2d2d;">
+        style="background-image: url('{{ asset('hero/background.png') }}'); background-size: cover; background-position: center; background-color: #2d2d2d;">
         <div>
             <img src="{{ asset('logo/shodwe_sem_fundo.png') }}" style="max-width: 300px">
         </div>
 
-        <div
-            class="w-full  sm:max-w-md mt-6 px-6 py-4 bg-white dark:bg-gray-800 shadow-md overflow-hidden sm:rounded-lg">
+        <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white dark:bg-gray-800 shadow-md overflow-hidden sm:rounded-lg">
             {{ $slot }}
         </div>
     </div>
+
+    <!-- Scripts -->
+    <script src="{{ asset('dist/assets/app.js') }}"></script>
+    <script src="https://js.stripe.com/v3/"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/1.1.3/sweetalert.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 </body>
-<script src="https://js.stripe.com/v3/"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/1.1.3/sweetalert.min.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+
 </html>
